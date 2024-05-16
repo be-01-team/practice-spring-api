e817c136bbf92a1bb84793329af2a0cefa3264f0
jinhanhan <hanjinhan89@gmail.com>
2024-05-15 18:56:06 -0700

    user list changed

--- src/main/java/com/example/StreamExample1.java
+++ src/main/java/com/example/StreamExample1.java
@@ -8,10 +8,10 @@ import java.util.stream.Collectors;
 public class StreamExample1 {
     public static void main(String[] args) {
         List<Customer> customers = new ArrayList<>();
-        customers.add(new Customer("C001", "이순신"));
-        customers.add(new Customer("C002", "아이유"));
-        customers.add(new Customer("C003", "카리나"));
-        customers.add(new Customer("C004", "윈터"));
+        customers.add(new Customer("C001", "장보고"));
+        customers.add(new Customer("C002", "최수종"));
+        customers.add(new Customer("C003", "핑클"));
+        customers.add(new Customer("C004", "옥주현"));
 
         // Q) StringUtils의 isLongName static 메소드를 이용하여 긴 이름의 손님들 이름을 출력
         customers.stream()
@@ -21,10 +21,10 @@ public class StreamExample1 {
                 .forEach(System.out::println);
 
         List<String> customerNames = Arrays.asList(
-                "이순신",
+                "장보고",
                 "민지",
-                "오해원",
-                "아이유"
+                "옥ㅅㅅ",
+                "최수종"
         );
 
         // Q) 손님들 이름을 가지고, 새로운 Customer List를 만들어라.
